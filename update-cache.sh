8a0e383e26b51de5e2a27a05353a785066a6e110
Dirk Meihoefer <dirk.meihoefer@wago.com>
2020-10-19 14:11:37 +0200

    Added scikit-learn.

--- update-cache.sh
+++ update-cache.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run --rm -t -v $PWD/cache:/root/.cache wagoautomation/wago-python:3.8-armv7 pip install $@
-sudo chmod -R 777 cache/
+docker run --rm -t --name cache-updater -v $PWD/.cache:/root/.cache wagoautomation/wago-python:3.8-armv7 pip install $@
+sudo chmod -R 777 .cache/
