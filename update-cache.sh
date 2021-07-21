ed7da5ea1e9cce4a22debbf01a64bc42d0d87e20
Dirk Meihoefer <dirk.meihoefer@wago.com>
2021-07-21 08:56:18 +0000

    Fixed access rights for shell scripts .

--- update-cache.sh
+++ update-cache.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run --rm -t --name cache-updater -v $PWD/.cache:/root/.cache wagoautomation/python-cache-updater pip install --upgrade pip $@
+sudo docker run --rm -t --name cache-updater -v $PWD/.cache:/root/.cache wagoautomation/python-cache-updater pip install --upgrade pip $@
 sudo chmod -R 777 .cache/
