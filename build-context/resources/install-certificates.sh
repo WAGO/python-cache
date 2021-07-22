ffb7f5a95a66cf1886908be2236a967c565529da
Dirk Meihoefer <dirk.meihoefer@wago.com>
2021-07-22 10:16:23 +0200

    Added Dockerfile.

--- build-context/resources/install-certificates.sh
+++ build-context/resources/install-certificates.sh
@@ -4,6 +4,5 @@ update-ca-certificates
 
 if [ -f "/usr/local/share/ca-certificates/cert_SSL-Intercept.crt" ]; then
     pip config set --global global.cert /usr/local/share/ca-certificates/cert_SSL-Intercept.crt
-    pip config set --global global.extra-index-url https://www.piwheels.org/simple
     git config --global http.sslCAInfo /usr/local/share/ca-certificates/cert_SSL-Intercept.crt
 fi
